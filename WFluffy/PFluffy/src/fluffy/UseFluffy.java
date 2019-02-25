34a4814e306f647642b51d2e7d1035ea80e727f7
jonas.freiburg <jonas.freiburghaus@he-arc.ch>
2019-02-25 15:37:46 +0100

    start MainGUI

--- WFluffy/PFluffy/src/fluffy/UseFluffy.java
+++ WFluffy/PFluffy/src/fluffy/UseFluffy.java
@@ -1,9 +1,11 @@
 package fluffy;
 
+import fluffy.userinterface.MainGUI;
+
 public class UseFluffy {
 
 	public static void main(String[] args) {
-		System.out.println("Romain");
+		new MainGUI();
 	}
 
 }
